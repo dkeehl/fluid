12aebfcb6b80d77447e1cec3955def749d75cbde
Thomas Lin <dkeehl@gmail.com>
2020-08-04 14:00:43 +0800

    Basic graphic settings

---
+++ src/main.rs
@@ -0,0 +1,539 @@
+#![allow(unused)]
+
+use winit::{
+    event::*,
+    event_loop::{EventLoop, ControlFlow},
+    window::{Window, WindowBuilder},
+};
+use futures::executor::block_on;
+use cgmath::{Matrix4, SquareMatrix, InnerSpace};
+use std::mem;
+
+mod texture;
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let mut state = block_on(State::new(&window));
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => if !state.input(event) {
+                match event {
+                    WindowEvent::KeyboardInput { input, .. } => {
+                        match input {
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            } => *control_flow = ControlFlow::Exit,
+                            _ => {}
+                        }
+                    }
+                    WindowEvent::Resized(physical_size) => state.resize(*physical_size),
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        // event is &WindowEvent, so new_inner_size is &&mut PhysicalSize
+                        state.resize(**new_inner_size);
+                    }
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    _ => {}
+                }
+            }
+            Event::MainEventsCleared => window.request_redraw(),
+            Event::RedrawRequested(_) => {
+                state.update();
+                state.render();
+            }
+            _ => {}
+        }
+    })
+}
+
+struct State {
+    surface: wgpu::Surface,
+
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+
+    render_pipeline: wgpu::RenderPipeline,
+
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_indice: u32,
+
+    // diffuse_texture: texture::Texture,
+    diffuse_bind_group: wgpu::BindGroup,
+    uniform_bind_group: wgpu::BindGroup,
+    uniform: Uniform,
+    uniform_buffer: wgpu::Buffer,
+
+    camera: Camera,
+    camera_controller: CameraController,
+}
+
+impl State {
+    async fn new(window: &Window) -> State {
+        let surface = wgpu::Surface::create(window);
+
+        let adapter = wgpu::Adapter::request(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::Default,
+                compatible_surface: Some(&surface),
+            },
+            wgpu::BackendBit::SECONDARY, // Set to PRIMARY on newer machines
+        ).await.expect("No adapters found");
+
+        let (device, mut queue) = adapter.request_device(&wgpu::DeviceDescriptor {
+            extensions: wgpu::Extensions {
+                anisotropic_filtering: false,
+            },
+            limits: Default::default(),
+        }).await;
+
+        let size = window.inner_size();
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        let mut compiler = shaderc::Compiler::new().unwrap();
+
+        let vs_src = include_str!("shader.vert");
+        let vs_spirv = compiler.compile_into_spirv(vs_src, shaderc::ShaderKind::Vertex, "shader.vert", "main", None).unwrap();
+        let vs_data = wgpu::read_spirv(std::io::Cursor::new(vs_spirv.as_binary_u8())).unwrap();
+        let vs_module = device.create_shader_module(&vs_data);
+
+        let fs_src = include_str!("shader.frag");
+        let fs_spirv = compiler.compile_into_spirv(fs_src, shaderc::ShaderKind::Fragment, "shader.frag", "main", None).unwrap();
+        let fs_data = wgpu::read_spirv(std::io::Cursor::new(fs_spirv.as_binary_u8())).unwrap();
+        let fs_module = device.create_shader_module(&fs_data);
+
+        let diffuse_bytes = include_bytes!("happy-tree.png");
+        let (diffuse_texture, cmd_buffer) =
+            texture::Texture::from_bytes(&device, diffuse_bytes, "happy-tree.png")
+            .unwrap();
+
+        queue.submit(&[cmd_buffer]);
+
+        let texture_bind_group_layout = device.create_bind_group_layout(
+            &wgpu::BindGroupLayoutDescriptor {
+                bindings: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStage::FRAGMENT,
+                        ty: wgpu::BindingType::SampledTexture {
+                            multisampled: false,
+                            dimension: wgpu::TextureViewDimension::D2,
+                            component_type: wgpu::TextureComponentType::Uint,
+                        },
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStage::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler {
+                            comparison: false,
+                        },
+                    },
+                ],
+                label: Some("texture_bind_group_layout"),
+            }
+        );
+
+        let diffuse_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &texture_bind_group_layout,
+            bindings: &[
+                wgpu::Binding {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&diffuse_texture.view),
+                },
+                wgpu::Binding {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(&diffuse_texture.sampler),
+                },
+            ],
+            label: Some("diffuse_bind_group"),
+        });
+
+        let camera = Camera {
+            eye: (0.0, 4.0, 2.0).into(),
+            target: (0.0, 0.0, 0.0).into(),
+            up: cgmath::Vector3::unit_y(),
+            aspect: sc_desc.width as f32 / sc_desc.height as f32,
+            fovy: 45.0,
+            znear: 0.1,
+            zfar: 100.0,
+        };
+
+        let uniform_bind_group_layout = device.create_bind_group_layout(
+            &wgpu::BindGroupLayoutDescriptor {
+                bindings: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStage::VERTEX,
+                        ty: wgpu::BindingType::UniformBuffer {
+                            dynamic: false,
+                        },
+                    },
+                ],
+                label: Some("uniform_bind_group_layout"),
+            }
+        );
+
+        let mut uniform = Uniform::new();
+        uniform.update(&camera);
+
+        let uniform_buffer = device.create_buffer_with_data(
+            bytemuck::cast_slice(&[uniform]),
+            wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
+        );
+
+        let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &uniform_bind_group_layout,
+            bindings: &[
+                wgpu::Binding {
+                    binding: 0,
+                    resource: wgpu::BindingResource::Buffer {
+                        buffer: &uniform_buffer,
+                        range: 0..std::mem::size_of_val(&uniform) as wgpu::BufferAddress,
+                    },
+                },
+            ],
+            label: Some("uniform_bind_group"),
+        });
+
+        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            bind_group_layouts: &[&texture_bind_group_layout, &uniform_bind_group_layout],
+        });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            layout: &render_pipeline_layout,
+            vertex_stage: wgpu::ProgrammableStageDescriptor {
+                module: &vs_module,
+                entry_point: "main",
+            },
+            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+                module: &fs_module,
+                entry_point: "main",
+            }),
+            rasterization_state: Some(wgpu::RasterizationStateDescriptor {
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: wgpu::CullMode::Back,
+                depth_bias: 0,
+                depth_bias_slope_scale: 0.0,
+                depth_bias_clamp: 0.0,
+            }),
+            color_states: &[
+                wgpu::ColorStateDescriptor {
+                    format: sc_desc.format,
+                    color_blend: wgpu::BlendDescriptor::REPLACE,
+                    alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                    write_mask: wgpu::ColorWrite::ALL,
+                },
+            ],
+            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+            depth_stencil_state: None,
+            vertex_state: wgpu::VertexStateDescriptor {
+                index_format: wgpu::IndexFormat::Uint16,
+                vertex_buffers: &[Vertex::desc()],
+            },
+            sample_count: 1,
+            sample_mask: !0,
+            alpha_to_coverage_enabled: false,
+        });
+
+        let vertex_buffer = device.create_buffer_with_data(
+            bytemuck::cast_slice(VERTICE),
+            wgpu::BufferUsage::VERTEX,
+        );
+
+        let index_buffer = device.create_buffer_with_data(
+            bytemuck::cast_slice(INDICE),
+            wgpu::BufferUsage::INDEX,
+        );
+
+        State {
+            surface,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            render_pipeline,
+            vertex_buffer,
+            index_buffer,
+            num_indice: INDICE.len() as u32,
+            // diffuse_texture,
+            diffuse_bind_group,
+            uniform_bind_group,
+            uniform,
+            uniform_buffer,
+
+            camera,
+            camera_controller: CameraController::new(0.2),
+        }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        self.camera_controller.process_events(event)
+    }
+
+    fn update(&mut self) {
+        self.camera_controller.update_camera(&mut self.camera);
+        self.uniform.update(&self.camera);
+
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("update encoder"),
+        });
+
+        let buf = self.device.create_buffer_with_data(
+            bytemuck::cast_slice(&[self.uniform]),
+            wgpu::BufferUsage::COPY_SRC,
+        );
+
+        encoder.copy_buffer_to_buffer(
+            &buf, 0, &self.uniform_buffer, 0, mem::size_of::<Uniform>() as wgpu::BufferAddress
+        );
+
+        self.queue.submit(&[encoder.finish()]);
+    }
+
+    fn render(&mut self) {
+        let frame = self.swap_chain.get_next_texture()
+            .expect("Timeout getting texture");
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                color_attachments: &[
+                    wgpu::RenderPassColorAttachmentDescriptor {
+                        attachment: &frame.view,
+                        resolve_target: None,
+                        load_op: wgpu::LoadOp::Clear,
+                        store_op: wgpu::StoreOp::Store,
+                        clear_color: wgpu::Color { r: 0.1, g: 0.2, b: 0.3, a: 1.0 },
+                    }
+                ],
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
+            render_pass.set_bind_group(1, &self.uniform_bind_group, &[]);
+            render_pass.set_vertex_buffer(0, &self.vertex_buffer, 0, 0);
+            render_pass.set_index_buffer(&self.index_buffer, 0, 0);
+            render_pass.draw_indexed(0..self.num_indice, 0, 0..1);
+        }
+        self.queue.submit(&[encoder.finish()])
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+struct Vertex {
+    position: [f32; 3],
+    uv: [f32; 2],
+}
+
+unsafe impl bytemuck::Zeroable for Vertex {}
+unsafe impl bytemuck::Pod for Vertex {}
+
+impl Vertex {
+    const fn new(position: [f32; 3], uv: [f32; 2]) -> Vertex {
+        Vertex {
+            position,
+            uv,
+        }
+    }
+
+    fn desc<'a>() -> wgpu::VertexBufferDescriptor<'a> {
+        use std::mem::size_of;
+
+        wgpu::VertexBufferDescriptor {
+            stride: size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::InputStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttributeDescriptor {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float3,
+                },
+                wgpu::VertexAttributeDescriptor {
+                    offset: size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float2,
+                },
+            ],
+        }
+    }
+}
+
+const VERTICE: &[Vertex] = &[
+    Vertex::new([-0.0868241,  0.49240386,  0.0], [0.4131759, 0.00759614]   ),
+    Vertex::new([-0.49513406, 0.06958647,  0.0], [0.0048659444, 0.43041354]),
+    Vertex::new([-0.21918549, -0.44939706, 0.0], [0.28081453, 0.949397057] ),
+    Vertex::new([0.35966998,  -0.3473291,  0.0], [0.85967, 0.84732911]     ),
+    Vertex::new([0.44147372,  0.2347359,   0.0], [0.9414737, 0.2652641]    ),
+];
+
+const INDICE: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+];
+
+struct Camera {
+    eye: cgmath::Point3<f32>,
+    target: cgmath::Point3<f32>,
+    up: cgmath::Vector3<f32>,
+    aspect: f32,
+    fovy: f32,
+    znear: f32,
+    zfar: f32,
+}
+
+const GL_WGPU_SWAP: Matrix4<f32> = Matrix4::new(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, -1.0, 0.0,
+    0.0, 0.0, 0.0, 1.0,
+);
+
+impl Camera {
+    fn build_view_projection_matrix(&self) -> Matrix4<f32> {
+        let view = Matrix4::look_at(self.eye, self.target, self.up);
+        let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
+
+        proj * view
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+struct Uniform {
+    view_proj: Matrix4<f32>,
+}
+
+unsafe impl bytemuck::Zeroable for Uniform {}
+unsafe impl bytemuck::Pod for Uniform {}
+
+impl Uniform {
+    fn new() -> Uniform {
+        Uniform {
+            view_proj: Matrix4::identity(),
+        }
+    }
+
+    fn update(&mut self, cam: &Camera) {
+        self.view_proj = cam.build_view_projection_matrix();
+    }
+}
+
+struct CameraController {
+    speed: f32,
+    left: bool,
+    right: bool,
+    up: bool,
+    down: bool,
+    forward: bool,
+    backward: bool,
+}
+
+impl CameraController {
+    fn new(speed: f32) -> Self {
+        Self {
+            speed,
+            left: false,
+            right: false,
+            up: false,
+            down: false,
+            forward: false,
+            backward: false,
+        }
+    }
+
+    fn process_events(&mut self, event: &WindowEvent) -> bool {
+        match event {
+            WindowEvent::KeyboardInput {
+                input: KeyboardInput {
+                    state,
+                    virtual_keycode: Some(keycode),
+                    ..
+                },
+                ..
+            } => {
+                let pressed = *state == ElementState::Pressed;
+                match keycode {
+                    VirtualKeyCode::Space => {
+                        self.up = pressed;
+                        true
+                    }
+                    VirtualKeyCode::LShift => {
+                        self.down = pressed;
+                        true
+                    }
+                    VirtualKeyCode::W => {
+                        self.forward = pressed;
+                        true
+                    }
+                    VirtualKeyCode::S => {
+                        self.backward = pressed;
+                        true
+                    }
+                    VirtualKeyCode::A => {
+                        self.left = pressed;
+                        true
+                    }
+                    VirtualKeyCode::D => {
+                        self.right = pressed;
+                        true
+                    }
+                    _ => false,
+                }
+            }
+            _ => false,
+        }
+    }
+
+    fn update_camera(&self, cam: &mut Camera) {
+        let forward = cam.target - cam.eye;
+        let forward_norm = forward.normalize();
+        let forward_mag = forward.magnitude();
+
+        if self.forward && forward_mag > self.speed {
+            cam.eye += forward_norm * self.speed;
+        }
+
+        if self.backward {
+            cam.eye -= forward_norm * self.speed;
+        }
+
+        let right = forward_norm.cross(cam.up).normalize();
+        let forward = cam.target - cam.eye;
+        let forward_mag = forward.magnitude();
+
+        if self.right {
+            cam.eye = cam.target - (forward + right * self.speed).normalize() * forward_mag;
+        }
+
+        if self.left {
+            cam.eye = cam.target - (forward - right * self.speed).normalize() * forward_mag;
+        }
+    }
+}
+
